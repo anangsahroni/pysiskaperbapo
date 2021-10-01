86632a09ef69407dc0263ecd5b8bcd0ca32b649e
Anang Sahroni <48075471+anangsahroni@users.noreply.github.com>
2021-10-01 18:17:14 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='ejperbo',
+    version='0.1.0',
+    py_modules=['ejperbo'],
+    install_requires=[
+      'numpy'
+      'pandas',
+      'tqdm'
+    ],
+    entry_points='''
+        [console_scripts]
+        ejperbo=ejperbo:ejperbo
+    ''',
+)
